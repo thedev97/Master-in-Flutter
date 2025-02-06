58b9f5ec94a7873661a5d0aa442152e7cc671a44
Soumya <soumyamishra637@gmail.com>
2025-02-06 16:46:20 +0530

    Factory & Singleton code added

--- lib/Factory/factory.dart
+++ lib/Factory/factory.dart
@@ -1,19 +1,86 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+/*
+Key Points in the Code
+--------------------------------
+Singleton Pattern Using factory Constructor
+----------------
+The class Person is implementing the Singleton Pattern by ensuring only one instance (person1) is created.
+The factory constructor Person.p1() is used instead of a normal constructor to control the instance creation.
+static Person? person1;
+This static variable holds the single instance of the Person class.
+It is initially null and gets assigned when Person.p1() is called for the first time.
+Factory Constructor (factory Person.p1())
+
+The factory keyword is used in Dart constructors when we do not always create a new object.
+Here, Person.p1() checks if person1 is already created; if not, it creates a new instance.
+
+Using ??= (Null-Aware Assignment Operator)
+person1 ??= Person(15, "Soumya");
+??= means:
+"If person1 is null, assign it a new instance. Otherwise, keep the existing value."
+First time calling Person.p1():
+person1 is null, so it creates Person(15, "Soumya") and assigns it.
+On subsequent calls:
+person1 is not null, so it simply returns the existing instance.
+debugPrint(p1.name);
+
+Since p1 is assigned using Person.p1(), it holds the singleton instance.
+debugPrint(p1.name); prints "Kevin".*/
+
+/// Singleton Pattern Using factory Constructor
 class Person {
-  int age = 0;
+  int? age;
   String? name;
-  static Person? person1;
 
   Person(this.age, this.name);
 
+  static Person? person;
+
   factory Person.p1() {
-    person1 ??= Person(15, "Soumya");
-    return person1!;
+    person = Person(24, 'Kevin');
+    return person!;
   }
+}
+
+/// Conditional based objects creation
+class Animal {
+  String? sound;
+
+  Animal(this.sound);
+
+  factory Animal.create(String type) {
+    if (type == 'dog') {
+      return Dog('Bark');
+    } else if (type == 'cat') {
+      return Cat('Mew');
+    } else {
+      return Animal('Unknown');
+    }
+  }
+
+  void makeSound() {
+    debugPrint('Animal make $sound');
+  }
+}
+
+class Dog extends Animal {
+  Dog(String super.sound);
+}
+
+class Cat extends Animal {
+  Cat(String super.sound);
 }
 
 void main() {
   var p1 = Person.p1();
-  debugPrint(p1.name);
+  debugPrint('Name: ${p1.name} & Age: ${p1.age}');
+
+  var d1 = Animal.create('dog');
+  d1.makeSound();
+  var c1 = Animal.create('cat');
+  c1.makeSound();
+
+  var unknown = Animal.create('Unknown');
+  unknown.makeSound();
 }
--- lib/Singleton/singleton.dart
+++ lib/Singleton/singleton.dart
@@ -1,21 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_interview_ques/Equtable/equtable.dart';
+
+/*
+Key Differences: Normal create instance vs Singleton instance
+---------------------------------------------------------------------------------
+Object Creation:
+Normal Instance: A new instance is created every time the constructor is called.
+Singleton Instance: The instance is created only once, and any further calls return the same instance.
+Memory Usage:
+
+Normal Instance: Each object occupies its own memory, so multiple instances may consume more memory.
+Singleton Instance: Only one instance exists in memory, making it more memory efficient.
+State:
+
+Normal Instance: Each instance has its own state, meaning you can have different values for different instances.
+Singleton Instance: All references to the Singleton class share the same state.
+Access:
+
+Normal Instance: Each instance is independent, and accessing it requires creating a new object.
+Singleton Instance: The same instance can be accessed from anywhere in the application, providing a global point of access.
+ */
+/// Normal Instance
+class A{
+  String msg;
+  A({required this.msg});
+
+  void printMsg(){
+    debugPrint(msg);
+  }
+}
+
+/// Singleton Instance
+class B{
+  String? msg;
+  B._internal(this.msg);
+
+  static final B _instance = B._internal('Hello');
+
+  factory B(){
+    return _instance;
+  }
+
+  void printMsg(){
+    debugPrint(msg);
+  }
+}
+
+class Person{
+  int? age;
+  String? name;
+  Person(this.name, this.age);
+
+  static Person? person;
+
+  factory Person.p1(){
+    person = Person('Kevin', 25);
+    return person!;
+  }
+
+  void info(){
+    debugPrint('Name: $name & Age: $age');
+  }
+}
+
 
 void main(){
-  MySingleton a = MySingleton();
-  MySingleton b = MySingleton();
+  /*var a = A(msg: 'Hello');
+  var b = A(msg: 'Hello');
+  a.printMsg();
+  b.printMsg();*/
+
+  var a = B();
+  var b = B();
   if(a==b){
     debugPrint("true");
   }else{
     debugPrint("false");
   }
-}
 
-class MySingleton{
-  static final MySingleton _singleton = MySingleton._internal();
-
-  factory MySingleton() {
-    return _singleton;
-  }
-
-  MySingleton._internal();
+  var p1 = Person.p1();
+  p1.info();
 }
